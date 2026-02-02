719e75c1865e616793a8218473c3f3a8234d7217
EongGeo <sseungj99@gmail.com>
2026-02-02 10:08:58 +0900

    feat : PlayerStatManager, FSM, BaseSO, Player 일부 구현

---
+++ Assets/LSJ/02 Script/SO/MonsterBaseStatsSO.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "GameData/Monster/BaseStats", fileName = "MonsterBaseStats")]
+public class MonsterBaseStatsSO : ScriptableObject
+{
+    public string monsterName;
+
+    [Header("±‚∫ª Ω∫≈»")]
+    public float baseAttackPower;
+    public float baseMaxHP;
+    public float baseDefensivePower;
+}
---
+++ Assets/LSJ/02 Script/SO/PlayerBaseStatsSO.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public enum Tier { Stone, Bronze, Silver, Gold, Platinum, Diamond, Amethyst, Ruby, Brilliance}
+[CreateAssetMenu(menuName = "GameData/Player/BaseStats", fileName = "PlayerBaseStats")]
+public class PlayerBaseStatsSO : ScriptableObject
+{
+    [Header("기본 정보")]
+    public int baseLevel = 1;
+    public Tier baseTier = Tier.Stone;
+
+    [Header("기본 스탯")]
+    public float baseAttackPower = 10f;
+    public float baseMaxHP = 100f;
+    public float baseHPRegenPerSec = 1f;     // 초당 회복 ( % 아님)
+    public float baseCritRate = 0.05f;    // 5%
+    public float baseCritDamage = 1.5f;     // 150%
+    public float baseMaxMana = 100f;
+    public float baseManaRegenPerSec = 1f;
+    public float baseGoldMultiplier = 1f;       // 추가 골드 획득량 배율
+    public float baseExpMultiplier = 1f;       // 추가 경험치 배율
+    public float baseAttackSpeed = 1f;       // 공격속도 ( 이건 어떤 식으로 적용시킬 지 고민해봐야 할듯)
+    public float baseMoveSpeed = 5f;       // 배경 스크롤과 몬스터 속도에 영향
+
+}
